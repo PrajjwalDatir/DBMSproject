773079bd9a8f586315ba4d390351ca54292ef808
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2020-11-02 00:19:06 +0530

    updated project

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    &copy; 2015 Online Course Registration | By : <a href="http://www.phpgurukul.com/" target="_blank">PHPGURUKUL</a>
+                    &copy;  Online School Management | By : Abhishek Rai & Prajjwal Dattir
                 </div>
 
             </div>
